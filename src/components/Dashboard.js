5e1c74ca540b333330d8c45c8d17ab07ed3fcaef
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-08-28 14:17:50 +0200

    updated Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,4 @@
 import React from "react";
-import { MdOutlineFreeBreakfast } from "react-icons/md";
-import { MdOutlineLunchDining } from "react-icons/md";
-import { MdOutlineDinnerDining } from "react-icons/md";
-import { LuDessert } from "react-icons/lu";
-import { GiBowlOfRice } from "react-icons/gi";
-import { GiBread } from "react-icons/gi";
-import { RiFileList3Line } from "react-icons/ri";
 
 class Dashboard extends React.Component {
   constructor(props) {
